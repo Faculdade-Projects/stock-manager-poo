95ad6071ef37032767e533603642551a2fcaa903
Opyher | Janiel Maia <opyherarts@gmail.com>
2023-12-01 16:16:01 -0300

    FEAT (stock-manager) - backend: api e banco de dados criado

--- POO/Django/stock-manager/backend/stock/models.py
+++ POO/Django/stock-manager/backend/stock/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50 ,null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10 ,decimal_places=2, null=False, blank=False)
+    quantity = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- POO/Django/stock-manager/backend/stock/views.py
+++ POO/Django/stock-manager/backend/stock/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from . import models, serializers
 
 # Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductSerializer
